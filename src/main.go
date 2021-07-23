5877f383ba42d8ee4db2fb8fdc1840d34863cbf7
Matheus Luis <suetham_luis@hotmail.com>
2021-07-22 22:26:19 -0300

    Removeing Dead Code

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"time"
 
 	"github.com/allegro/bigcache/v3"
@@ -13,43 +12,6 @@ import (
 	"avenue/app/repository"
 	"avenue/app/service"
 )
-
-func PathParams(c *gin.Context) {
-	name := c.Param("name")
-	age := c.Param("age")
-
-	c.JSON(200, gin.H{
-		"name": name,
-		"age":  age,
-	})
-}
-
-func QueryString(c *gin.Context) {
-	name := c.Query("name")
-	age := c.Query("age")
-
-	c.JSON(200, gin.H{
-		"name": name,
-		"age":  age,
-	})
-}
-
-func PostHomePage(c *gin.Context) {
-	body := c.Request.Body
-	value, err := ioutil.ReadAll(body)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	c.JSON(200, gin.H{
-		"message": string(value),
-	})
-}
-
-func HomePage(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello World",
-	})
-}
 
 func main() {
 	fmt.Println("Hello World")
